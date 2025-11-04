ec9684f204e332ba0e24fa0f3307435fa53ead9f
Alessandra <savi.souza@escola.pr.gov.br>
2025-11-04 09:09:31 -0300

    04/11

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+const botcaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
